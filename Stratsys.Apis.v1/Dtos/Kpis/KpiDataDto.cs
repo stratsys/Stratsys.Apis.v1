694e07b918c3b31da37f862d928d8469bb59b93c
David Ernstsson <david.ernstsson@stratsys.se>
2014-02-18 16:30:26 +0100

    RYC-22533 Få ut namn och position på måttkolumn vid måttdata. Ska kunna använda detta även för att sätta måttdata givet viss namn/position på kolumn för att göra världen lite enklare.

--- Stratsys.Apis.v1/Dtos/Kpis/KpiDataDto.cs
+++ Stratsys.Apis.v1/Dtos/Kpis/KpiDataDto.cs
@@ -5,6 +5,8 @@
         public string KpiId { get; set; }
         public string DepartmentId { get; set; }
         public string KpiColumnId { get; set; }
+        public string KpiColumnName { get; set; }
+        public int? KpiColumnPosition { get; set; }
         public string Date { get; set; }
         public double? Value { get; set; }
     }

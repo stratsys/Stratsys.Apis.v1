36aa332aff2b09483c4732de48a91ac94a6594ad
David Ernstsson <david.ernstsson@stratsys.se>
2014-07-30 16:25:00 +0200

    RYC-23810 Api för att spara Users

--- Stratsys.Apis.v1/Dtos/Organization/UserDto.cs
+++ Stratsys.Apis.v1/Dtos/Organization/UserDto.cs
@@ -1,11 +1,24 @@
 ﻿namespace Stratsys.Apis.v1.Dtos.Organization
 {
-    public class UserDto
+    public class UserDto : UserProperties
     {
         public string Id { get; set; }
+    }
+
+    public class CreateUserDto : UserProperties
+    {
+        public string Password { get; set; }
+    }
+
+    public class UserProperties
+    {
+        public string MainGroupId { get; set; }
+        public string MainDepartmentId { get; set; }
         public string Username { get; set; }
+        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
+        public string PhoneRegular { get; set; }
+        public string PhoneMobile { get; set; }
     }
 }

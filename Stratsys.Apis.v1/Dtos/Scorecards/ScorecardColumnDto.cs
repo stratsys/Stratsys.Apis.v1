931e436f05eee43ffd0bfdba744fb219bb89d6e9
David Nyberg <david.nyberg@stratsys.se>
2015-06-08 13:36:39 +0200

    RYC-28462 Stratsys api: Styrkortskolumner ska även ge ut namn på styrkort

--- Stratsys.Apis.v1/Dtos/Scorecards/ScorecardColumnDto.cs
+++ Stratsys.Apis.v1/Dtos/Scorecards/ScorecardColumnDto.cs
@@ -1,9 +1,13 @@
-﻿namespace Stratsys.Apis.v1.Dtos.Scorecards
+﻿using System;
+
+namespace Stratsys.Apis.v1.Dtos.Scorecards
 {
     public class ScorecardColumnDto
     {
         public string Id { get; set; }
+        [Obsolete("use property Scorecard")]
         public string ScorecardId { get; set; }
+        public ScorecardDto Scorecard { get; set; }
         public string Name { get; set; }
         public string Definition { get; set; }
         public int Index { get; set; }

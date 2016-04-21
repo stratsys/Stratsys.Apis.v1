b2588fbca3865b07888412ceac6ce94c94864839
David <david.ernstsson@stratsys.se>
2016-04-21 14:09:37 +0200

    RYC-31704 Utöka api/v1/reportlist med information kring enhet och genomförande

--- Stratsys.Apis.v1/Dtos/StratsysApps/UserReportListDto.cs
+++ Stratsys.Apis.v1/Dtos/StratsysApps/UserReportListDto.cs
@@ -6,5 +6,16 @@
         public string Url { get; set; }
         public DeadlineViewModelDto Deadline { get; set; }
         public string ReportPeriod { get; set; }
+        public string DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public UserReportListItemProgressDto ReportProgress { get; set; }
+    }
+
+    public class UserReportListItemProgressDto
+    {
+        public double PercentageReady { get; set; }
+        public double PercentageFinished { get; set; }
+        public bool ShouldRender { get; set; }
+        public bool ShouldShowBothReadyAndPassed { get; set; }
     }
 }

83de41e0a104b028661f70ab11dac2e5cf0cedd8
torbjorn-hansson-stratsys <torbjorn.hansson@stratys.se>
2020-06-09 08:35:22 +0200

    Nya dto:er för stöd för länkar på noder.

---
+++ Stratsys.Apis.v1/Dtos/Scorecards/DocumentInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stratsys.Apis.v1.Dtos.Scorecards
+{
+    public class DocumentInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime UploadedDate { get; set; }
+        public string Url { get; set; }
+    }
+}

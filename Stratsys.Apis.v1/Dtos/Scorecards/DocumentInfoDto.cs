b9aaeb7d88a9df4730ffd7e80301940c4ebca5e6
torbjorn-hansson-stratsys <torbjorn.hansson@stratys.se>
2020-06-16 14:03:57 +0200

    ParentId kan vara null för mappar på root-nivå. Lagt till Dto-suffix.

--- Stratsys.Apis.v1/Dtos/Scorecards/DocumentInfoDto.cs
+++ Stratsys.Apis.v1/Dtos/Scorecards/DocumentInfoDto.cs
@@ -2,7 +2,7 @@
 
 namespace Stratsys.Apis.v1.Dtos.Scorecards
 {
-    public class DocumentInfo
+    public class DocumentInfoDto
     {
         public int Id { get; set; }
         public string Name { get; set; }

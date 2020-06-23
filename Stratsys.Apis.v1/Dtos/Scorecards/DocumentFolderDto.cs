20dd955025521180c0ef74265be01b8f279d11d5
Torbjörn Hansson <torbjorn.hansson@stratsys.se>
2020-06-23 13:14:27 +0200

    STR-5200 Utöka API Dto med stöd för länkar på noder
    Co-authored-by: torbjorn-hansson-stratsys <torbjorn.hansson@stratys.se>

---
+++ Stratsys.Apis.v1/Dtos/Scorecards/DocumentFolderDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Stratsys.Apis.v1.Dtos.Scorecards
+{
+    public class DocumentRootFolderDto
+    {
+        public IList<DocumentFolderDto> Folders { get; set; }
+        public IList<DocumentInfoDto> Documents { get; set; }
+    }
+
+    public class DocumentFolderDto : DocumentRootFolderDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ParentId { get; set; }
+    }
+}
